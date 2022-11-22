4040ea96b21383d129b9f5c3346f6318bc2a8d1f
황선민 <92971339+hwangseonmin@users.noreply.github.com>
2022-11-22 16:53:32 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,59 @@
+
+//100% 주기
+		$(document).ready(function () {
+			screenCheck();
+
+			$('.scroll-control .one').click(function () {
+				$.scrollify.move('#s-one');
+			});
+			$('.scroll-control .two').click(function () {
+				$.scrollify.move('#s-two');
+			});
+			$('.scroll-control .three').click(function () {
+				$.scrollify.move('#s-three');
+			});
+			$('.scroll-control .four').click(function () {
+				$.scrollify.move('#s-four');
+			});
+		});
+
+		$(window).on('resize', function () {
+			screenCheck();
+		});
+
+		function applyScroll() {
+			$.scrollify({
+				section: '.scroll',
+				sectionName: 'section-name',
+				//standardScrollElements: 'section',
+				easing: 'easeOutExpo',
+				scrollSpeed: 1100,
+				offset: 0,
+				scrollbars: true,
+				setHeights: true,
+				overflowScroll: true,
+				updateHash: false,
+				touchScroll: true,
+			});
+		}
+
+		function screenCheck() {
+			var deviceAgent = navigator.userAgent.toLowerCase();
+			var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+			if (agentID || $(window).width() <= 1024) {
+				// its mobile screen
+				$.scrollify.destroy();
+				$('section').removeClass('scroll').removeAttr('style');
+				$.scrollify.disable();
+			} else {
+				// its desktop
+				$('section').addClass('scroll');
+				applyScroll();
+				$.scrollify.enable();
+			}
+		}
+
+        //슬라이드쇼
 $(document).ready(function () {
     $(".nav > ul > li").mouseover(function () {
         $(this).find(".submenu").stop().slideDown(200);
@@ -16,14 +72,15 @@ $(document).ready(function () {
             currentIndex = 0;
         }
 
-        var slidePosition = currentIndex * (-700) + "px";
+        var slidePosition = currentIndex * (-750) + "px";
         $(".slide-cont").animate({ top: slidePosition }, 800);
 
     }, 3000);
 
 
 
-});
+}
+);
 
 
 
