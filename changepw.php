190ac1c90402ef2aff67e0b0fc8d4f2a4e376299
ihg1130 <116706652+ihg1130@users.noreply.github.com>
2022-11-29 14:22:11 +0900

    Add files via upload

--- changepw.php
+++ changepw.php
@@ -12,7 +12,7 @@
             <div class="registerTitle">비밀번호 변경</div>
             <div class="changePw">
             <form action="member_process.php?mode=changepw" method="post">
-                <input type="hidden" name="userid" value="<?= $_GET['id']?>">
+                <input type="hidden" name="id" value="<?= $_GET['id']?>">
                 <p>새 비밀번호 입력 : <input type="pw1"  name="pw1" size="30"></p>
                 <p class="findEmail">새 비밀번호 확인 : <input type="pw2"  name="pw2" size="30"></p>
                 <div class="findBtn">
